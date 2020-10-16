6901dfe1cf5e9c7e084988870b31ca8bd59cd87e
Ian Wren <ian.wren81@gmail.com>
2020-10-16 16:24:46 -0700

    Added reference for api calls and api key.

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+api.openweathermap.org/data/2.5/weather?q= + "" + &appid=bb70df7726fdccc57ce65df7344701bc
+
+api.openweathermap.org/data/2.5/uvi?lat= + "" + &lon= + "" + &appid=bb70df7726fdccc57ce65df7344701bc
