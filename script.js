89ba9f4ae394cce0b6126d73e3e8d2a4902fb0f6
Ian Wren <ian.wren81@gmail.com>
2020-10-31 20:09:40 -0700

    Added a UV index ajax call and commented out button listeners and ajax calls to view styling as editing to prevent bugs.

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let btnEl = document.querySelector(".button");
+let listieEl = document.querySelector(".listie");
 let filler = userInput
 
 
-$.ajax({
-  url: "https://api.openweathermap.org/data/2.5/weather?q= + filler + &appid=bb70df7726fdccc57ce65df7344701bc"
-  ,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+// $.ajax({
+//   url: "https://api.openweathermap.org/data/2.5/weather?q= + filler + &appid=bb70df7726fdccc57ce65df7344701bc"
+//   ,
+//   method: "GET"
+// }).then(function(response) {
+//   console.log(response);
+// });
 
-api.openweathermap.org/data/2.5/weather?q= + "" + &appid=bb70df7726fdccc57ce65df7344701bc
+// $.ajax({
+//   url: "https://api.openweathermap.org/data/2.5/uvi?lat= + filler + &appid=bb70df7726fdccc57ce65df7344701bc"
+//   ,
+//   method: "GET"
+// }).then(function(response) {
+//   console.log(response);
+// });
 
-api.openweathermap.org/data/2.5/uvi?lat= + "" + &lon= + "" + &appid=bb70df7726fdccc57ce65df7344701bc
 
-
-btnEl.addEventListener("click", function(event) {
-    event.preventDefault();
-    if(event.target.matches("button")) {
-      var item = document.createElement("div");
-      item.textContent = cities[event.target.parentElement.id];
-      citylist.append(item);
-    }
-  });
+// btnEl.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     if(event.target.matches("button")) {
+//       var item = document.createElement("div");
+//       item.textContent = listieEl[event.target.parentElement.id];
+//       listieEl.append(item);
+//     }
+//   });
