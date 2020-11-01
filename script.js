d039ecbbd6ecc723abdd06f35b686339f9432a91
Ian Wren <ian.wren81@gmail.com>
2020-10-31 21:03:19 -0700

    Fixed jQuery link in index, and made sure ajax call worked to console log data from endpoint using variable string 'seattle' in order to see returned data for parsing to index html in future.

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
+$(document).ready(function() {
+
 let btnEl = document.querySelector(".button");
 let listieEl = document.querySelector(".listie");
-let filler = userInput
+// let filler = userInput;
+// let city = "seattle"
 
+// let  = 
+function searchWeather() {
 
+    let city = "seattle";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=bb70df7726fdccc57ce65df7344701bc";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // 
+        //     
+        //       createRow(response);
+    })
+};
+//  });
+// $(".table tbody").append(rowEl);
+// });
+
+searchWeather();
 // $.ajax({
-//   url: "https://api.openweathermap.org/data/2.5/weather?q= + filler + &appid=bb70df7726fdccc57ce65df7344701bc"
+//   url: "https://api.openweathermap.org/data/2.5/uvi?lat= + "seattle" + &appid=bb70df7726fdccc57ce65df7344701bc"
 //   ,
 //   method: "GET"
 // }).then(function(response) {
 //   console.log(response);
 // });
 
-// $.ajax({
-//   url: "https://api.openweathermap.org/data/2.5/uvi?lat= + filler + &appid=bb70df7726fdccc57ce65df7344701bc"
-//   ,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response);
-// });
 
-
-// btnEl.addEventListener("click", function(event) {
+// btnEl.$("onclick", function(event) {
 //     event.preventDefault();
 //     if(event.target.matches("button")) {
 //       var item = document.createElement("div");
-//       item.textContent = listieEl[event.target.parentElement.id];
+//       item.textContent = listieEl[event.text];
 //       listieEl.append(item);
 //     }
 //   });
+})
